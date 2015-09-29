0388b07682d2d32b72e9cc401370bf25b52916cc
chauhn03 <chauhn.it@gmail.com>
2015-09-29 09:20:18 +0700

    merge

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -4,19 +4,6 @@
  * Footer widgets
  */
 
-<<<<<<< HEAD
-if(!is_active_sidebar('sidebar-2')){
-    return;
-}
-?>
-
-<div id="supplementary">
-    <div id="footer-sidebar" class="footer-widgets widget-area clear" role="complementary">
-        <?php dynamic_sidebar('sidebar-2')?>
-    </div> <!-- #footers-sidebar -->
-</div> <!-- #supplementary-->
-
-=======
 if (!is_active_sidebar('sidebar-2')){
     return;
 }
@@ -26,4 +13,4 @@ if (!is_active_sidebar('sidebar-2')){
         <?php dynamic_sidebar('sidebar-2'); ?>
     </div> <!-- #footer-sidebar -->
 </div> <!-- #supplementary -->
->>>>>>> develop
+
